46ebca775fd64576cd150e34f03c6d7d0a741ba0
PRAJWAL K K <162725373+prajwal-kk98@users.noreply.github.com>
2024-08-19 08:19:03 +0530

    Create 1.cpp

---
+++ patterns/1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    int rows=5,col=4;
+    for(i=1;i<=rows;++i){
+        for(j=1;j<=col;++j){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
